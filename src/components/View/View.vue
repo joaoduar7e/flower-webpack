50df054f0e18d700903e97784c869ae05383276d
joaoduar7e <joao.duarte@edu.unipar.br>
2023-01-25 23:42:16 -0300

    ajuste view

--- src/components/View/View.vue
+++ src/components/View/View.vue
@@ -1,16 +1,37 @@
 <template>
   <div>
-    {{ id }}
+    {{ flower.id }}
+    {{ flower.name }}
+    {{ flower.authors }}
   </div>
 </template>
 
 <script>
+import api from "../../../service/api";
+
 export default {
   props: ["id"],
   data() {
     return {
-      flower:[]
+      flower: []
     };
+  },
+
+  created() {
+    this.getFlower();
+  },
+
+  methods: {
+    getFlower() {
+      api
+        .get("/flowers/" + this.id)
+        .then(res => {
+          this.flower = res.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   }
 };
 </script>
